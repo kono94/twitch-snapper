38fbcd5d693124d2fa193059c7cd0f89711166ac
Jan Löwenstrom <jan.loewenstrom@web.de>
2022-02-01 19:06:26 +0100

    track lirik's chat messages

---
+++ app.js
@@ -0,0 +1,52 @@
+import 'dotenv/config'
+import { Client as IRCClient } from 'tmi.js'
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: "lul_snapper",
+    password: process.env.IRC_OAUTH
+  },
+  channels: [
+    "lirik"
+  ]
+};
+
+// Create a client with our options
+const client = new IRCClient(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  console.log(commandName)
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+   // console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
---
+++ twitch_api_notes.md
@@ -0,0 +1,26 @@
+### Clips
+POST https://api.twitch.tv/helix/clips
+
+broadcaster_id  : string
+has_delay : boolean
+
+Returns:
+edit_url: string
+id: string
+
+
+### User Info
+
+GET https://api.twitch.tv/helix/users
+
+id: id
+
+
+### Video (last boradcast)
+
+GET https://api.twitch.tv/helix/videos
+
+user_id: string
+
+sort: string -> Sort order of the videos. Valid values: "time", "trending", "views". Default: "time".
+type: string -> type of video. Valid values: "all", "upload", "archive", "highlight". Default: "all".
