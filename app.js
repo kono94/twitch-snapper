d3a7b24f788062bdfaf721b95c53b594a8c60105
Your Name <you@example.com>
2022-04-16 00:25:51 +0200

    add request for viewer count and simple winston logger

--- app.js
+++ app.js
@@ -1,52 +1,61 @@
-import 'dotenv/config'
-import { Client as IRCClient } from 'tmi.js'
+import "dotenv/config"
+import { Client as IRCClient } from "tmi.js"
+import axios from "axios"
+import Logger from "./util/Logger.js"
 
+const CHANNEL = "asmongold"
+let nChatters = 0
+
+Logger.info("kek")
 // Define configuration options
 const opts = {
   identity: {
     username: "lul_snapper",
-    password: process.env.IRC_OAUTH
+    password: process.env.IRC_OAUTH,
   },
-  channels: [
-    "lirik"
-  ]
-};
-
-// Create a client with our options
-const client = new IRCClient(opts);
-
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
-
-// Connect to Twitch:
-client.connect();
-
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-
-  console.log(commandName)
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-   // console.log(`* Unknown command ${commandName}`);
-  }
+  channels: [CHANNEL],
 }
 
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+// Create a client with our options
+const client = new IRCClient(opts)
+// Connect to Twitch:
+client.connect()
+
+// Called every time a message comes in
+const onMessageHandler = (target, context, msg, self) => {
+  if (self) {
+    return
+  } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const trimmedMsg = msg.trim()
+  console.log(trimmedMsg)
+}
+
+const fetchViewerCount = async () => {
+  console.log("Fetching viewer count...")
+  let chatterCount = 0
+  try {
+    const res = await axios.get(
+      `https://tmi.twitch.tv/group/user/${CHANNEL}/chatters?`
+    )
+    chatterCount = res.data.chatter_count
+  } catch (err) {
+    console.log(err)
+  }
+  return chatterCount
 }
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+const onConnectedHandler = async (addr, port) => {
+  console.log(`* Connected to ${addr}:${port}`)
+  nChatters = fetchViewerCount()
 }
+
+setInterval(() => {
+  nChatters = fetchViewerCount()
+}, 60 * 60)
+
+// Register our event handlers (defined below)
+client.on("message", onMessageHandler)
+client.on("connected", onConnectedHandler)
