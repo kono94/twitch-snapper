8a4a6cc6f5c6001e2f341cfe74fc70f15bb79055
Jan Loewenstrom <jan.loewenstrom@web.de>
2023-08-12 00:09:38 +0200

    add experimental test suite and fix pylint in hook

---
+++ test/triggerTest.py
@@ -0,0 +1,37 @@
+import asyncio
+import sys
+import unittest
+from pathlib import Path
+from unittest.mock import MagicMock
+
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+from snapper.app import init_twitchAPI
+from snapper.observer import StreamObserver
+
+
+class TestSuit:
+    async def async_setup(self):
+        self.twitchAPI = await init_twitchAPI()
+        print("setup done")
+
+    async def test_trigger(self):
+        print("test_trigger")
+        observer = StreamObserver("lirik", self.twitchAPI)
+        observer._create_clip = MagicMock()
+
+
+class TestRunner(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.testSuit = TestSuit()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(cls.testSuit.async_setup())
+
+    def test_1(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.testSuit.test_trigger())
+
+
+if __name__ == "__main__":
+    unittest.main()
