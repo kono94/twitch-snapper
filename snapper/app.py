d0d0634d97287002ba843c005234a437e68b0069
Jan Löwenstrom <jan.loewenstrom@web.de>
2023-10-15 22:59:49 +0200

    add time frame selection (today, last 7 days, last month)

--- snapper/app.py
+++ snapper/app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from quart import Quart, jsonify, render_template, request
 from sqlalchemy import asc, desc
 
@@ -32,6 +34,15 @@ async def api_clips():
     page = int(request.args.get("page", 1))
     per_page = int(request.args.get("per_page", 20))
     sort_by = request.args.get("sort_by", "latest")
+    last_timestamp_iso: str | None = request.args.get("last_timestamp_iso", None)
+
+    if last_timestamp_iso != None:
+        print(last_timestamp_iso)
+        utc_timestamp = datetime.fromisoformat(
+            last_timestamp_iso
+        )  # Convert the string to a datetime object
+    else:
+        utc_timestamp = None
 
     # Determine sort order
     if sort_by == "latest":
@@ -42,7 +53,7 @@ async def api_clips():
         order_by = desc(Clip.created)  # Default sort
 
     # Fetch and sort clips from the database, skipping the clips for previous pages
-    clips = await get_by_page_and_sort(Clip, page, per_page, order_by)
+    clips = await get_by_page_and_sort(Clip, page, per_page, order_by, utc_timestamp)
 
     return jsonify([clip.to_dict() for clip in clips])
 
