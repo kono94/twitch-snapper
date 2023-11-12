050528c66d1fb9a4973462c3bfb05b357f4e969a
Jan Löwenstrom <jan.loewenstrom@web.de>
2023-11-12 13:55:23 +0100

    Revert "add infinite scrolling of clips and sort by"
    This reverts commit bae7c0e75087bcca19e54eb4f017d30363e61946.

--- snapper/app.py
+++ snapper/app.py
@@ -1,7 +1,6 @@
-from quart import Quart, jsonify, render_template, request
-from sqlalchemy import asc, desc
+from quart import Quart, render_template
 
-from snapper.database import Clip, Stream, get_all, get_by_page_and_sort
+from snapper.database import Clip, Stream, get_all
 from snapper.main import PROJECT_ROOT
 
 app: Quart = Quart(
@@ -25,26 +24,6 @@ async def index():
 async def clips():
     clips = await get_all(Clip)
     return await render_template("clips.html", clips=clips)
-
-
-@app.route("/api/clips", methods=["GET"])
-async def api_clips():
-    page = int(request.args.get("page", 1))
-    per_page = int(request.args.get("per_page", 20))
-    sort_by = request.args.get("sort_by", "latest")
-
-    # Determine sort order
-    if sort_by == "latest":
-        order_by = desc(Clip.created)
-    elif sort_by == "keyword_count":
-        order_by = desc(Clip.keyword_count)
-    else:
-        order_by = desc(Clip.created)  # Default sort
-
-    # Fetch and sort clips from the database, skipping the clips for previous pages
-    clips = await get_by_page_and_sort(Clip, page, per_page, order_by)
-
-    return jsonify([clip.to_dict() for clip in clips])
 
 
 @app.route("/about")
