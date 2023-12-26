e4d0abb90b2b228da472d67c17b2e0a637e6582e
Jan Löwenstrom <jan.loewenstrom@web.de>
2023-12-26 13:17:00 +0100

    add encrypted .env files and deploy pipeline

---
+++ deployment/update_via_ci.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+cd "$(dirname "$0")"
+
+# Check if the VERSION argument is provided
+if [ -z "$1" ]; then
+    echo "Error: VERSION argument not provided."
+    echo "Usage: $0 VERSION"
+    exit 1
+fi
+
+VERSION_TAG=$1
+
+git fetch --tags
+git checkout tags/$VERSION_TAG
+
+echo "Provided version is: $VERSION; Building container..."
+docker build -t snapper:$VERSION_TAG ../.
+
+echo "Decypting secrets"
+cat .env.compose.encrypted | base64 -d | age --decrypt -i key.txt > .env.compose
+cat .docker.env.encrypted | base64 -d | age --decrypt -i key.txt > .docker.env
+
+echo "Re-starting docker compose"
+docker compose up -d
